3e622c632ebc8868883de0acd0c9171d2bc6f9d3
Carlos Gallego <carlos.341810357@gmail.com>
2020-05-12 17:09:38 -0500

    cambios JS

--- src/main.js
+++ src/main.js
@@ -6,3 +6,19 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+  tasks: [
+    {name : "ir a clase de prog web 1", selected: false},
+    {name : "realizar el taller de prog web 1", selected: true},
+    {name : "presetar el taller de rpog web 1", selected: false},
+    {name : "presetar el taller de rpog web 2", selected: false},
+  ]
+methods: {
+   addTask()
+    let task = {
+      name: this.newName,
+      selected: false
+    }
+    this.tasks.push(task);
+    this.newName = '';
+  }
